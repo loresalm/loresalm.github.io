12d6165ad98a1bf92e9f60dc79d298dfcf868903
Lorenzo Salmina <lorenzosalmina@macbook-pro-di-lorenzo-6.home>
2019-08-17 21:00:15 +0200

    ss

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster()
+const synth = new Tone.AMSynth().toMaster()
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease('C4', '8n')
-console.log('this is a test')
+//attach a listener to the keyboard events
+document.querySelector('tone-keyboard').addEventListener('noteon', e => {
+  synth.triggerAttack(e.detail.name)
+})
 
-
+document.querySelector('tone-keyboard').addEventListener('noteoff', e => {
+  synth.triggerRelease()
+})
 
 //----------------------------------------
 

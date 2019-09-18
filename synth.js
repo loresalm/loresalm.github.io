fe01cedfbf1b0071fd673f257a35e9c462f02147
lorenzosalmina <lorenzo.salmina@epfl.ch>
2019-09-18 21:48:38 +0200

    first test

--- synth.js
+++ synth.js
@@ -4,20 +4,24 @@ let bassSynth
 
 function preload(){
 
-	bassSynth = new Tone.Synth().toMaster()
+	bassSynth = new Tone.MembraneSynth().toMaster()
 
 	loopBeat = new Tone.Loop(song, '4n')
 
 }
 
 function setup() {
-
-
+	Tone.Transport.bpm.value = 180
 	Tone.Transport.start()
 	loopBeat.start(0)
 }
 
 function song(time){
 	console.log(time)
-	bassSynth.triggerAttackRelease('C4', '8n', time)
+	bassSynth.triggerAttackRelease('c1', '8n', time)
+
+	if (time >= 12){
+		Tone.Transport.bpm.value = 70
+	}
+
 }
